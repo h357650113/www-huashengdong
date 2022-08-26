f11c3a6298b3b31babc05e6e8f396ef109c510bd
hsd <h357650113@hotmail.com>
2022-08-26 09:08:29 +0800

    feature: add router

---
+++ src/src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.scss'
+
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="logo">huashengdong.com</div>
+            <div className="email">email</div>
+        </div>
+    )
+}
---
+++ src/src/Pages/Default/Default.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../../Components/Header/Header'
+import './index.scss'
+
+export default function Default() {
+    return (
+        <div className="page-default">
+            <Header />
+        </div>
+    )
+}
