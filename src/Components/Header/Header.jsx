7b030a1eac9f3289d1c793fbeb4edefd4ad7152d
hsd <h357650113@hotmail.com>
2022-08-30 15:04:43 +0800

    feature: add github in header component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import './index.scss'
 import emailSrc from '../../asset/icon/email.png'
+import githubSrc from '../../asset/icon/github.png'
 
 export default function Header() {
+    const handleGithubClick = () => {
+        window.open('https://github.com/h357650113')
+    }
     return (
         <div className="header">
             <div className="logo">huashengdong.com</div>
-            <div className="email">
-                <img src={emailSrc} alt="察纳雅言" />
-                <div className="text">Email</div>
+            <div className="tips">
+                <div className="github" onClick={handleGithubClick}>
+                    <img src={githubSrc} alt="h357650113 github" />
+                </div>
+                <div className="email">
+                    <img src={emailSrc} alt="察纳雅言" />
+                    <div className="text">Email</div>
+                </div>
             </div>
         </div>
     )
