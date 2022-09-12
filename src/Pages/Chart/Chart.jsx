6093c33a0cbc9f1513c9dc64fad21ab1eb25d7bc
hsd <h357650113@hotmail.com>
2022-09-12 12:52:00 +0800

    feature: 30 day chart count

--- src/Pages/Chart/Chart.jsx
+++ src/Pages/Chart/Chart.jsx
@@ -13,9 +13,9 @@ export default function Chart() {
             <Header />
             <SearchBar hasBack />
             <div className="charts-wrap">
-                <h3>日访问量：</h3>
+                <h3>30天日访问量：</h3>
                 <DateViewsChart />
-                <h3>Blog问量：</h3>
+                <h3>30天Blog问量：</h3>
                 <BlogViewsChart />
                 <h3>区域统计：</h3>
                 <AreaCountChart />
